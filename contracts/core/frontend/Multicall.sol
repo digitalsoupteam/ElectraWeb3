5e63794455db196caf368e32017b38d8292acbc6
inveker <alex.korolev.1995@gmail.com>
2023-10-11 16:45:28 +0300

    add Multicall contract

---
+++ contracts/core/frontend/Multicall.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { MulticallUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
+
+import { IAddressBook } from "../../interfaces/IAddressBook.sol";
+
+contract Multicall is UUPSUpgradeable, MulticallUpgradeable {
+    address public addressBook;
+
+    function initialize(address _addressBook) public initializer {
+        addressBook = _addressBook;
+    }
+
+    function _authorizeUpgrade(address) internal view override {
+        IAddressBook(addressBook).enforceIsProductOwner(msg.sender);
+    }
+}
