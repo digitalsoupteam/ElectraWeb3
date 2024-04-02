0764842c96ee7b5c99f74a39890cc87549d2261f
inveker <alex.korolev.1995@gmail.com>
2024-04-02 18:39:50 +0300

    add already deployed check

--- deploy/600-FlexStakingStrategyImplementation.ts
+++ deploy/600-FlexStakingStrategyImplementation.ts
@@ -3,10 +3,13 @@ import { DeployFunction } from 'hardhat-deploy/types'
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { ethers, deployments } = hre
-  const { deploy, get } = deployments
+  const { deploy, get, getOrNull } = deployments
 
   const signers = await ethers.getSigners()
   const deployer = signers[0]
+
+  const alreadyDeployed = await getOrNull('FlexStakingStrategyImplementation') != null
+  if(alreadyDeployed) return
 
   const deployment = await deploy('FlexStakingStrategyImplementation', {
     contract: 'FlexStakingStrategy',
